a48269d2ab9b03c265d9aa18162febc6777bb577
Biarslan <thebiarslan@gmail.com>
2024-03-11 13:56:43 +0300

    feat: implement OpenAPI and add swagger-ui

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule } from '@nestjs/swagger';
+import { getSwaggerDoc } from './utils/getSwaggerDocs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
 
-  const PORT = config.get('PORT') || 4000;
+  const apiDoc = await getSwaggerDoc();
+  SwaggerModule.setup('api', app, apiDoc);
 
+  const PORT = config.get('PORT') || 4000;
   await app.listen(PORT);
 }
 bootstrap();
