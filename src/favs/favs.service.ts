6a7b54c54c741ed1bb334b1b1ad9f954b600c2cb
Biarslan <thebiarslan@gmail.com>
2024-03-25 23:49:55 +0300

    feat: update favorites service

--- src/favs/favs.service.ts
+++ src/favs/favs.service.ts
@@ -5,67 +5,102 @@ import { DatabaseService } from 'src/database/database.service';
 export class FavsService {
   constructor(private readonly databaseService: DatabaseService) {}
   async findAll() {
-    const favs = await this.databaseService.favorites.findMany();
+    const favs = await this.databaseService.favorites.findFirst({
+      select: {
+        artists: { select: { id: true, name: true, grammy: true } },
+        tracks: {
+          select: {
+            id: true,
+            duration: true,
+            name: true,
+            artistId: true,
+            albumId: true,
+          },
+        },
+        albums: {
+          select: { id: true, name: true, artistId: true, year: true },
+        },
+      },
+    });
 
-    return favs;
-  }
-  addTrack(id: string) {
-    // const db = this.databaseService.getDB();
-    // const track = db.tracks.find((track) => track.id === id);
-    // if (track === undefined) return undefined;
-    // db.favorites.tracks.push(id);
-    // this.databaseService.updateDB(db);
-    return true;
-  }
-  deleteTrack(id: string) {
-    // const db = this.databaseService.getDB();
-    // const track = db.tracks.find((track) => track.id === id);
-    // if (track === undefined) return undefined;
-    // const updatedFavTracks = db.favorites.tracks.filter(
-    //   (track) => track !== id,
-    // );
-    // db.favorites.tracks = updatedFavTracks;
-    // this.databaseService.updateDB(db);
-    return true;
+    return favs || (await this.databaseService.favorites.create({ data: {} }));
   }
 
-  addAlbum(id: string) {
-    // const db = this.databaseService.getDB();
-    // const album = db.albums.find((album) => album.id === id);
-    // if (album === undefined) return undefined;
-    // db.favorites.albums.push(id);
-    // this.databaseService.updateDB(db);
-    return true;
+  async addTrack(id: string) {
+    const track = await this.databaseService.track.findUnique({
+      where: { id },
+    });
+    if (track === null) return undefined;
+    const favs =
+      (await this.databaseService.favorites.findFirst()) ||
+      (await this.databaseService.favorites.create({ data: {} }));
+
+    return await this.databaseService.favorites.update({
+      where: { id: favs.id },
+      data: { tracks: { connect: { id } } },
+    });
   }
-  deleteAlbum(id: string) {
-    // const db = this.databaseService.getDB();
-    // const album = db.albums.find((album) => album.id === id);
-    // if (album === undefined) return undefined;
-    // const updatedFavAlbums = db.favorites.albums.filter(
-    //   (album) => album !== id,
-    // );
-    // db.favorites.albums = updatedFavAlbums;
-    // this.databaseService.updateDB(db);
+  async deleteTrack(id: string) {
+    const favs =
+      (await this.databaseService.favorites.findFirst()) ||
+      (await this.databaseService.favorites.create({ data: {} }));
+    await this.databaseService.favorites.update({
+      where: { id: favs.id },
+      data: { tracks: { disconnect: { id } } },
+      select: { tracks: true },
+    });
     return true;
   }
 
-  addArtist(id: string) {
-    // const db = this.databaseService.getDB();
-    // const artist = db.artists.find((artist) => artist.id === id);
-    // if (artist === undefined) return undefined;
-    // db.favorites.artists.push(id);
-    // this.databaseService.updateDB(db);
+  async addAlbum(id: string) {
+    const album = await this.databaseService.album.findUnique({
+      where: { id },
+    });
+    if (album === null) return undefined;
+    const favs =
+      (await this.databaseService.favorites.findFirst()) ||
+      (await this.databaseService.favorites.create({ data: {} }));
+
+    return await this.databaseService.favorites.update({
+      where: { id: favs.id },
+      data: { albums: { connect: { id } } },
+    });
+  }
+  async deleteAlbum(id: string) {
+    const favs =
+      (await this.databaseService.favorites.findFirst()) ||
+      (await this.databaseService.favorites.create({ data: {} }));
+    await this.databaseService.favorites.update({
+      where: { id: favs.id },
+      data: { albums: { disconnect: { id } } },
+      select: { albums: true },
+    });
     return true;
   }
-  deleteArtist(id: string) {
-    // const db = this.databaseService.getDB();
-    // const artist = db.artists.find((artist) => artist.id === id);
-    // if (artist === undefined) return undefined;
-    // const updatedFavArtists = db.favorites.artists.filter(
-    //   (artist) => artist !== id,
-    // );
-    // db.favorites.artists = updatedFavArtists;
-    // this.databaseService.updateDB(db);
+
+  async addArtist(id: string) {
+    const artist = await this.databaseService.artist.findUnique({
+      where: { id },
+    });
+    if (artist === null) return undefined;
+    const favs =
+      (await this.databaseService.favorites.findFirst()) ||
+      (await this.databaseService.favorites.create({ data: {} }));
+
+    return await this.databaseService.favorites.update({
+      where: { id: favs.id },
+      data: { artists: { connect: { id } } },
+    });
+  }
+  async deleteArtist(id: string) {
+    const favs =
+      (await this.databaseService.favorites.findFirst()) ||
+      (await this.databaseService.favorites.create({ data: {} }));
+    await this.databaseService.favorites.update({
+      where: { id: favs.id },
+      data: { artists: { disconnect: { id } } },
+      select: { artists: true },
+    });
     return true;
   }
 }
